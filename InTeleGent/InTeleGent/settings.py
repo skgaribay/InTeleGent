d2bb85fb5e6e352516c826c1a91e09d12183f323
Sean Kristian Garibay <sggaribay@up.edu.ph>
2021-12-16 11:32:05 +0800

    removed Secret key

--- InTeleGent/InTeleGent/settings.py
+++ InTeleGent/InTeleGent/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6snc1*+d%ahpr&u*m1#$!b_p4#s2!^kxmqsddq!4ej&^7o=75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
