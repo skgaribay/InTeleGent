364c247aa217792098d9d4c5fecf738eab304d1a
Sean Kristian Garibay <sggaribay@up.edu.ph>
2021-12-30 16:14:43 +0800

    heroku prep

--- InTeleGent/InTeleGent/settings.py
+++ InTeleGent/InTeleGent/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os 
+import django_heroku
 
 from pathlib import Path
 
@@ -130,3 +131,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
